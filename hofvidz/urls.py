52fe705db84a700c47feebf115358ea56ac219bf
Allan Burns <allanlburns@yahoo.com>
2019-05-02 20:52:02 -0400

    Implement authentication views and authentication templates, create models, create and run migrations, register models with admin, and set login redirect.

--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from halls import views
 from django.conf.urls.static import static
@@ -7,5 +8,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    # AUTH
+    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
