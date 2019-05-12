1331321f1984edc811ab4a99599c0383223d9471
Allan Burns <allanlburns@yahoo.com>
2019-05-12 14:06:33 -0400

    Add jumbotron to home page. Style Login and Signup. Display recent galleries and popular galleries on home page.

--- halls/views.py
+++ halls/views.py
@@ -13,7 +13,9 @@ import requests
 YOUTUBE_API_KEY = 'AIzaSyAuXSRbu6lTth-tD3yCGBDrjNHZ7UzrVc0'
 
 def home(request):
-	return render(request, 'halls/home.html')
+	recent_halls = Hall.objects.all().order_by('-id')[:3]
+	popular_halls = [Hall.objects.get(pk=1), Hall.objects.get(pk=4), Hall.objects.get(pk=5)]
+	return render(request, 'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
 
 def dashboard(request):
 	halls = Hall.objects.filter(user=request.user)
